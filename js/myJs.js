b0eb0a96f3f7abc52e4940f77fbf9160fdc0cd88
Huỳnh Thị Ngọc Thoại <42405264+ngonhatduy@users.noreply.github.com>
2025-10-25 21:15:38 +0700

    test

--- js/myJs.js
+++ js/myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'He luu cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        title: 'Hello!',
+        text: 'Trả lời câu hỏi này nào =))',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Anh biết abg đẹp trai mà =))',
         html: true,
         width: 900,
         padding: '3em',
@@ -108,26 +108,13 @@ $('#yes').click(function() {
               left top
               no-repeat
             `,
-        showCancelButton: true,
-        cancelButtonText: "Thôi ngại lém :<<",
+        showCancelButton: false,
+        cancelButtonText: "=))",
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonColor: '#fe8a71',
         cancelButtonColor: '#f6cd61',
-        confirmButtonText: 'Gửi cho tớ <3'
+        confirmButtonText: 'Lại nào'
     }).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                width: 900,
-                confirmButtonText: 'Okiiiii lun <3',
-                background: '#fff url("img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu 300.000',
-                text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
-                confirmButtonColor: '#83d0c9',
-                onClose: () => {
-                    window.location = 'http://fb.com';
-                  }
-            })
-        }
     })
 })
